c3486df3d240f290fe812811b2390ad3655eca73
andrejpa <apavlo00fesb.hr>
2023-10-11 18:30:34 +0200

    hel2

--- Vjezba1/Vjezba1/Vjezba1.c
+++ Vjezba1/Vjezba1/Vjezba1.c
@@ -1,7 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
+#define MAX 50
+
+typedef struct {
+	char ime[MAX];
+	char prezime[MAX];
+	int bodovi;
+}student;
 
 int main()
 {
-	printf("Hello world");
+	student stud;
+	FILE *dat;
+	int noRows;
+
+	dat = fopen("lista.txt", "r");
+
+	while (!feof)
+	{
+		if (fgetc(dat) == '\n')
+		{
+			noRows++;
+		}
+
+	}
+	printf("%d", noRows);
 	return 0;
 }
