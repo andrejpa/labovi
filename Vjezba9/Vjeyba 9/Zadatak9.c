b3140ff13cae836dc126dc4b294ee7392617757f
andrejpa <147499784+andrejpa@users.noreply.github.com>
2024-01-12 12:24:25 +0100

    Zadatak9.c

--- Vjezba9/Vjeyba 9/Zadatak9.c
+++ Vjezba9/Vjeyba 9/Zadatak9.c
@@ -1,4 +1,4 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -11,8 +11,41 @@ typedef struct _node {
     Position right;
 } Node;
 
+Position initNode(int);
+Position ins(Position, int);
 
-Position createNode(int value) {
+int treeHeight(Position);
+void CurrentLevel(Position, int);
+void levelOrder(Position);
+
+int replace(Position);
+int RandomNumberGen();
+
+Position insertNumbers(Position);
+
+
+int main() {
+    srand(time(0));
+    
+    Position root = NULL;
+    Position rootRandomized = NULL;
+
+    root = insertNumbers(root);
+    levelOrder(root);
+    
+    replace(root);
+    printf("\n");
+    levelOrder(root);
+
+    rootRandomized = ins(rootRandomized, RandomNumberGen());
+    for (int i = 0; i < 10; i++) {
+        ins(rootRandomized, RandomNumberGen());
+    }
+    printf("\n");
+    levelOrder(rootRandomized);
+
+}
+Position initNode(int value) {
     Position newNode = NULL;
     newNode = (Position)malloc(sizeof(Node));
     if (!newNode) {
@@ -26,39 +59,44 @@ Position createNode(int value) {
 }
 Position ins(Position root, int value) {
     if (root == NULL)
-        return createNode(value);
+        return initNode(value);
 
-    if (value <= root->value)
+    if (value < root->value)
         root->left = ins(root->left, value);
-    else if (value > root->value)
+    else if (value >= root->value)
         root->right = ins(root->right, value);
 
     return root;
 }
-int levelOrder(Position root) {
+int treeHeight(Position root) {
     if (root == NULL)
         return 0;
-
-    Position queue[100] = { 0 };
-    int front = 0, rear = 0;
-
-    queue[rear++] = root;
-
-    while (front < rear) {
-        Position current = queue[front++];
-
-        printf("%d ", current->value);
-
-        if (current->left != NULL)
-            queue[rear++] = current->left;
-
-        if (current->right != NULL)
-            queue[rear++] = current->right;
+    else {
+        int leftHeight = treeHeight(root -> left);
+        int rightHeight = treeHeight(root -> right);
+        
+        if (leftHeight > rightHeight)
+            return (leftHeight + 1);
+        else 
+            return (rightHeight + 1);
     }
-    return 1;
 }
+void CurrentLevel(Position root, int height) {
+    if (root == NULL)
+        return;
+        
+    if (height == 1)
+        printf("%d ", root -> value);
+    else if (height > 1) {
+        CurrentLevel(root -> right, height - 1);
+        CurrentLevel(root -> left, height - 1);
 
-
+    }
+}
+void levelOrder(Position root) {
+    for (int i = 1; i <= treeHeight(root); i++)
+        CurrentLevel(root, i);
+}
 int replace(Position root) {
     if (root == NULL) {
         return 0;
@@ -67,23 +105,15 @@ int replace(Position root) {
     int leftValue = replace(root->left);
     int rightValue = replace(root->right);
 
-    int originalValue = root->value;
-
+    int childValue = root->value;
     root->value = leftValue + rightValue;
 
-    return originalValue + root->value;
+    return childValue + root->value;
 }
-
-int ran() {
+int RandomNumberGen() {
     return (rand() % 80) + 10;
 }
-int main() {
-
-    srand(time(0));
-    
-    Position root = NULL;
-    Position rootRan = NULL;
-
+Position insertNumbers(Position root){
     root = ins(root, 2);
     ins(root, 5);
     ins(root, 7);
@@ -94,19 +124,5 @@ int main() {
     ins(root, 2);
     ins(root, 3);
     ins(root, 7);
-
-    levelOrder(root);
-    
-    replace(root);
-    printf("\n");
-    levelOrder(root);
-
-
-    rootRan = ins(rootRan, ran());
-    for (int i = 0; i < 10; i++) {
-        ins(rootRan, ran());
-    }
-    printf("\n");
-    levelOrder(rootRan);
-
+    return root;
 }
